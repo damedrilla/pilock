3ce6937adc4aaf6b1bd6c127fd693daa733dd75c
dota1g <alfredtheslugger@gmail.com>
2024-03-27 08:48:58 +0800

    Initial commit

---
+++ xdd.py
@@ -0,0 +1,49 @@
+import requests
+import timecheck
+import json
+from datetime import datetime
+
+    
+while True:
+    schedres = requests.get('http://152.42.167.108/api/schedules')
+    currentSchedule = requests.get('http://152.42.167.108/api/schedules/current')
+    parseSched = json.loads(schedres.text)
+    parseCurrentSched = json.loads(currentSchedule.text)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    current_weekday = now.strftime('%A')
+    print("\nCurrent time and day is: " +  current_time + " " + current_weekday)
+    
+    def isStudAllowedtoEnter(section):
+        sectionFound = False
+        schedules = []
+        for sch in range(len(parseSched['schedules'])):
+            if str(parseSched['schedules'][sch]['section']).lower() == section.lower():
+                sectionFound = True
+                if sectionFound:
+                    schedules.append(parseSched['schedules'][sch])
+
+        if sectionFound:
+            hasClassesToday = False
+            for REALsched in range(len(schedules)):
+                print(schedules[REALsched]['days'])
+                if str(schedules[REALsched]['days']) == str(current_weekday):
+                    canIEnter = timecheck.isThisTheTime(schedules[REALsched]['time_start'], schedules[REALsched]['time_end'], current_time)
+                    if canIEnter:
+                        print("get in homie")
+                        hasClassesToday = True
+                        break
+                    else:
+                        print("wait for your turn bozo")
+                        hasClassesToday = True
+                        break
+            
+            if not hasClassesToday:
+                print("you don't have classes in this room for this day retard")
+                         
+        print(str(schedules))
+        if not sectionFound:
+            print("Invalid section")
+    section = input('Section: ')
+    isStudAllowedtoEnter(section)  
+    
