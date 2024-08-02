71926cfe35a6a86442ba29264a411019cb364cb6
dota1g <alfredtheslugger@gmail.com>
2024-08-02 23:07:38 +0800

    unlock on exitEventListener

--- exitEventListener.py
+++ exitEventListener.py
@@ -1,11 +1,13 @@
 from LCDcontroller import userExit
 import coloredlogs, logging
+from lock_state import changeLockState
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='pilock.log', encoding='utf-8', level=logging.DEBUG)
 coloredlogs.install(level="DEBUG", logger=logger)
 def exitListener():
     while True:
         uid = input('Waiting')
+        changeLockState('unlock')
         logger.info("A user goes out of the laboratory.")
         userExit()
     
