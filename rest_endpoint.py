30d09459bb15d0073e91e5a4837c7fad15d72a14
damedrilla <fhaus.nozomi@gmail.com>
2024-08-20 20:07:03 +0800

    added sanity check on rest endpoint

--- rest_endpoint.py
+++ rest_endpoint.py
@@ -1,11 +1,13 @@
 from flask import Flask, json, jsonify
 from lock_state import changeLockState
+from internetCheck import isInternetUp
 from flask_cors import CORS, cross_origin
 
 from getCurrentSchedule import currentSchedule
 
+
 def endpoint():
-    
+
     api = Flask(__name__)
     CORS(api, support_credentials=True)
 
@@ -14,9 +16,15 @@ def endpoint():
         changeLockState("unlock")
         print("Door unlocked successfully")
         return json.dumps({"success": True}), 201
-    @api.route("/schedule", methods=['GET'])
+
+    @api.route("/schedule", methods=["GET"])
     @cross_origin(supports_credentials=True)
     def getSched():
         return jsonify(currentSchedule()), 200
-        
+    
+    @api.route("/sanity_check", methods=["GET"])
+    @cross_origin(supports_credentials=True)
+    def getInternet():
+        return jsonify({'localMode': isInternetUp()}), 200
+
     api.run(host="0.0.0.0", port=5000)
