0c0ebdeed43f48424b1f23187d01ec355a7f21df
damedrilla <fhaus.nozomi@gmail.com>
2024-08-20 20:07:03 +0800

    cors update

--- rest_endpoint.py
+++ rest_endpoint.py
@@ -1,9 +1,13 @@
 from flask import Flask, json, jsonify
 from lock_state import changeLockState
+from flask_cors import CORS, cross_origin
+
 from getCurrentSchedule import currentSchedule
 
 def endpoint():
+    
     api = Flask(__name__)
+    CORS(api, support_credentials=True)
 
     @api.route("/unlock", methods=["POST"])
     def unlockDoor():
@@ -11,6 +15,7 @@ def endpoint():
         print("Door unlocked successfully")
         return json.dumps({"success": True}), 201
     @api.route("/schedule", methods=['GET'])
+    @cross_origin(supports_credentials=True)
     def getSched():
         return jsonify(currentSchedule()), 200
         
