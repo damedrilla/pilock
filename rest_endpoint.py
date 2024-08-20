87515fb631b05340d48c722a9765f20ef5ab2bcd
damedrilla <fhaus.nozomi@gmail.com>
2024-08-20 20:07:03 +0800

    rest endpoint for potential local web app

--- rest_endpoint.py
+++ rest_endpoint.py
@@ -1,6 +1,6 @@
 from flask import Flask, json
 from lock_state import changeLockState
-
+from getCurrentSchedule import currentSchedule
 
 def endpoint():
     api = Flask(__name__)
@@ -10,5 +10,8 @@ def endpoint():
         changeLockState("unlock")
         print("Door unlocked successfully")
         return json.dumps({"success": True}), 201
-
+    @api.route("/schedule", methods=['GET'])
+    def getSched():
+        return json.dumps(currentSchedule()), 200
+        
     api.run(host="0.0.0.0", port=5000)
